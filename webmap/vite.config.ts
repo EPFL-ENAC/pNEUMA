e183d5337e8780bffdfef0147eaa1444e39bb783
lmptfy <62747966+lmptfy@users.noreply.github.com>
2023-04-20 17:09:07 +0200

    Changed vite configs

--- webmap/vite.config.ts
+++ webmap/vite.config.ts
@@ -5,11 +5,20 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '^/data.*': {
+        target: 'http://127.0.0.1:8000/data',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/data/, '')
+      }
+    }
+  },
   plugins: [vue(), vuetify()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/it4r-webmap/'
+  base: '/'
 })
